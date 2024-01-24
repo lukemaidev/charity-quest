4fe43a70ecc3bb3d21c2d721792b0301daa9b8ff
Luke Mai <mailong.c8.cva@gmail.com>
2024-01-24 14:19:14 +1000

    Adding correct providers

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 export default function Page() {
   return (
-    <div>
-      <h1>Page</h1>
+    <div className="">
+
     </div>
   );
 }
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
+
